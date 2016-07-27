12635eb5a52e8b316d04e5749b549285257ecd15
compae <gserranojc@gmail.com>
2016-07-27 11:02:38 +0200

    added more modes

--- src/main/java/org/apache/kafka/connect/es/KafkaElasticSearchSinkConnectorConfig.java
+++ src/main/java/org/apache/kafka/connect/es/KafkaElasticSearchSinkConnectorConfig.java
@@ -6,38 +6,94 @@ import org.apache.kafka.common.config.AbstractConfig;
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.common.config.ConfigDef.Importance;
 import org.apache.kafka.common.config.ConfigDef.Type;
+import org.apache.kafka.common.config.ConfigException;
+
+import java.util.HashMap;
 
 
 public class KafkaElasticSearchSinkConnectorConfig extends AbstractConfig {
 
     public static final String CLUSTER_NAME = "elasticsearch.cluster.name";
-    public static final String CLUSTER_NAME_DOC = "A comma separated Elastic search cluster including port with a :. For example localhost:9300";
+    public static final String CLUSTER_NAME_DOC = "Elastic search cluster name";
 
     public static final String HOSTS = "elasticsearch.hosts";
-    public static final String HOSTS_DOC = "elasticsearch.hosts";
+    public static final String HOSTS_DOC = "A comma separated Elastic search hosts including port with a :. For " +
+            "example localhost:9300";
 
     public static final String BULK_SIZE = "elasticsearch.bulk.size";
-    public static final String BULK_SIZE_DOC = "The number of messages to be bulk indexed into elasticsearch.";
+    public static final String BULK_SIZE_DOC = "The number of messages to be bulk indexed into elasticsearch";
 
     public static final String DOCUMENT_NAME = "elasticsearch.document.name";
-    public static final String DOCUMENT_NAME_DOC = "elasticsearch.document.name";
+    public static final String DOCUMENT_NAME_DOC = "The topicIndexes type of Elasticsearch index";
 
     public static final String TOPICS = "topics";
-    public static final String TOPICS_DOC = "topics of kafka";
+    public static final String TOPICS_DOC = "topics in kafka";
 
     public static final String INDEX = "elasticsearch.index";
     public static final String INDEX_DOC = "The name of elasticsearch index";
 
+    public static final String DELETE_ID_FIELD = "elasticsearch.delete.idField";
+    public static final String DEFAULT_DELETE_ID_FIELD = "id";
+    public static final String DELETE_ID_FIELD_DOC = "Id field when the action type is delete, update or upsert";
 
-    static ConfigDef config = new ConfigDef().define(HOSTS, Type.STRING, Importance.HIGH,HOSTS_DOC)
+    public static final String ACTION_TYPE = "action.type";
+    public static final String ACTION_TYPE_DOC = "The action type against how the messages should be processed." +
+            " Default is: index. The following options are available:\n"
+            + "index : Creates documents in ES with the values in the received message\n"
+            + "update : Update documents in ES with the values in the received message based on id field\n" +
+            "upsert : Update and create if not exists documents in ES with the values in the received " +
+            "message based on id field set\n"
+            + "delete : Deletes documents from ES based on id field in the received message";
+
+
+    static ConfigDef config = new ConfigDef().define(HOSTS, Type.STRING, Importance.HIGH, HOSTS_DOC)
             .define(CLUSTER_NAME, Type.STRING, Importance.HIGH, CLUSTER_NAME_DOC)
-            .define(INDEX, Type.STRING,Importance.HIGH, INDEX_DOC)
-            .define(BULK_SIZE, Type.INT,Importance.HIGH, BULK_SIZE_DOC)
-            .define(TOPICS,Type.STRING,Importance.HIGH, TOPICS_DOC)
-            .define(DOCUMENT_NAME, Type.STRING,Importance.HIGH, DOCUMENT_NAME_DOC);
+            .define(INDEX, Type.STRING, Importance.HIGH, INDEX_DOC)
+            .define(BULK_SIZE, Type.INT, Importance.HIGH, BULK_SIZE_DOC)
+            .define(TOPICS, Type.STRING, Importance.HIGH, TOPICS_DOC)
+            .define(DOCUMENT_NAME, Type.STRING, Importance.HIGH, DOCUMENT_NAME_DOC)
+            .define(DELETE_ID_FIELD, Type.STRING, Importance.MEDIUM, DELETE_ID_FIELD_DOC)
+            .define(ACTION_TYPE, Type.STRING, Importance.MEDIUM, ACTION_TYPE_DOC);
 
     public KafkaElasticSearchSinkConnectorConfig(Map<String, String> props) {
         super(config, props);
+    }
+
+    public enum ActionType {
+
+        INDEX("index"), DELETE("delete"), UPDATE("update"), UPSERT("upsert");
+
+        private String actionType;
+        private static Map<String, ActionType> ACTIONS = init();
+
+        ActionType(String actionType) {
+            this.actionType = actionType;
+        }
+
+        public String toValue() {
+            return actionType;
+        }
+
+        public static ActionType toType(String value) {
+            return ACTIONS.get(value);
+        }
+
+        public static Map<String, ActionType> init() {
+            Map<String, ActionType> actions = new HashMap<>();
+            ActionType[] types = values();
+            for (ActionType type : types) {
+                actions.put(type.name().toLowerCase(), type);
+            }
+            return actions;
+        }
+    }
+
+    public ActionType getActionType() {
+        try {
+            return ActionType.toType(getString(ACTION_TYPE));
+        } catch (ConfigException e) {
+            return ActionType.INDEX;
+        }
     }
 
 }
--- src/main/java/org/apache/kafka/connect/es/KafkaElasticSearchSinkTask.java
+++ src/main/java/org/apache/kafka/connect/es/KafkaElasticSearchSinkTask.java
@@ -2,6 +2,7 @@ package org.apache.kafka.connect.es;
 
 import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.config.ConfigException;
 import org.apache.kafka.connect.errors.ConnectException;
 import org.apache.kafka.connect.errors.RetriableException;
 import org.apache.kafka.connect.sink.SinkRecord;
@@ -9,12 +10,16 @@ import org.apache.kafka.connect.sink.SinkTask;
 import org.elasticsearch.action.bulk.BulkItemResponse;
 import org.elasticsearch.action.bulk.BulkRequestBuilder;
 import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.client.Client;
+import org.elasticsearch.client.Requests;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.apache.kafka.connect.es.KafkaElasticSearchSinkConnectorConfig.*;
 
 import java.net.InetAddress;
 import java.util.*;
@@ -22,7 +27,6 @@ import java.util.*;
 /**
  * ElasticsearchSinkTask is a Task that takes records loaded from Kafka and sends them to
  * another system.
- *
  */
 public class KafkaElasticSearchSinkTask extends SinkTask {
 
@@ -30,8 +34,12 @@ public class KafkaElasticSearchSinkTask extends SinkTask {
 
     Integer bulkSize;
     String documentName;
+    String idField;
     Client client;
-    Map<String, String> mapping;
+    Map<String, String> topicIndexes;
+    Map<String, String> topicMappings;
+    ActionType actionType;
+    KafkaElasticSearchSinkConnectorConfig config;
 
     @Override
     public String version() {
@@ -46,12 +54,27 @@ public class KafkaElasticSearchSinkTask extends SinkTask {
     @Override
     public void start(Map<String, String> props) {
 
-        mapping = new HashMap<>(0);
+        try {
+            config = new KafkaElasticSearchSinkConnectorConfig(props);
+        } catch (ConfigException e) {
+            throw new ConnectException("Couldn't start " + KafkaElasticSearchSinkConnector.class.getName() + " due to configuration error.", e);
+        }
+
+        topicIndexes = new HashMap<>(0);
+        topicMappings = new HashMap<>(0);
         String clusterName = props.get(KafkaElasticSearchSinkConnectorConfig.CLUSTER_NAME);
         String hosts = props.get(KafkaElasticSearchSinkConnectorConfig.HOSTS);
         documentName = props.get(KafkaElasticSearchSinkConnectorConfig.DOCUMENT_NAME);
         String topics = props.get(KafkaElasticSearchSinkConnectorConfig.TOPICS);
         String indexes = props.get(KafkaElasticSearchSinkConnectorConfig.INDEX);
+        String mappingTypes = props.get(KafkaElasticSearchSinkConnectorConfig.DOCUMENT_NAME);
+        actionType = config.getActionType();
+
+        if (props.containsKey(KafkaElasticSearchSinkConnectorConfig.DELETE_ID_FIELD)) {
+            idField = props.get(KafkaElasticSearchSinkConnectorConfig.DELETE_ID_FIELD);
+        } else {
+            idField = KafkaElasticSearchSinkConnectorConfig.DEFAULT_DELETE_ID_FIELD;
+        }
 
         try {
             bulkSize = Integer.parseInt(props.get(KafkaElasticSearchSinkConnectorConfig.BULK_SIZE));
@@ -62,21 +85,22 @@ public class KafkaElasticSearchSinkTask extends SinkTask {
         List<String> hostsList = new ArrayList<>(Arrays.asList(hosts.replaceAll(" ", "").split(",")));
         List<String> topicsList = Arrays.asList(topics.replaceAll(" ", "").split(","));
         List<String> indexesList = Arrays.asList(indexes.replaceAll(" ", "").split(","));
+        List<String> mappingTypesList = Arrays.asList(mappingTypes.replaceAll(" ", "").split(","));
 
         if (topicsList.size() != indexesList.size()) {
             throw new ConnectException("The number of indexes should be the same as the number of topics");
         }
 
         for (int i = 0; i < topicsList.size(); i++) {
-            mapping.put(topicsList.get(i), indexesList.get(i));
+            topicIndexes.put(topicsList.get(i), indexesList.get(i));
+            topicMappings.put(topicsList.get(i), mappingTypesList.get(i));
         }
 
         try {
-            Settings settings = Settings.settingsBuilder()
-                    .put("cluster.name", clusterName).build();
+            Settings settings = Settings.settingsBuilder().put("cluster.name", clusterName).build();
 
             client = TransportClient.builder().settings(settings).build();
-            log.info("topic"+ topics);
+            log.info("topic" + topics);
             for (String host : hostsList) {
                 String address;
                 Integer port;
@@ -88,7 +112,7 @@ public class KafkaElasticSearchSinkTask extends SinkTask {
                 } catch (Exception e) {
                     port = 9300;
                 }
-                log.info("address "+address+"port "+port);
+                log.info("address " + address + "port " + port);
                 ((TransportClient) client).addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(address), port));
             }
         } catch (Exception e) {
@@ -105,19 +129,40 @@ public class KafkaElasticSearchSinkTask extends SinkTask {
     @Override
     public void put(Collection<SinkRecord> sinkRecords) {
         try {
-            List<SinkRecord> records = new ArrayList<SinkRecord>(sinkRecords);
+            List<SinkRecord> records = new ArrayList<>(sinkRecords);
             for (int i = 0; i < records.size(); i++) {
                 BulkRequestBuilder bulkRequest = client.prepareBulk().setRefresh(Boolean.TRUE);
                 for (int j = 0; j < bulkSize && i < records.size(); j++, i++) {
                     SinkRecord record = records.get(i);
 
-                    Map<String, Object> jsonMap = ( Map<String, Object>)record.value();
+                    Map<String, Object> jsonMap = (Map<String, Object>) record.value();
 
-                    String index = mapping.get(record.topic());
+                    String index = topicIndexes.get(record.topic());
+                    String mappingType = topicMappings.get(record.topic());
+                    String id;
 
-                    bulkRequest.add(
-                            client.prepareIndex(index, documentName).setSource(jsonMap)
-                    );
+                    switch (actionType) {
+                        case INDEX:
+                            bulkRequest.add(client.prepareIndex(index, mappingType).setSource(jsonMap));
+                            break;
+                        case DELETE:
+                            id = (String) jsonMap.get(idField);
+                            bulkRequest.add(Requests.deleteRequest(index).type(documentName).id(id));
+                            break;
+                        case UPDATE:
+                            id = (String) jsonMap.get(idField);
+                            bulkRequest.add(new UpdateRequest(index, documentName, id).doc(jsonMap));
+                            break;
+                        case UPSERT:
+                            id = (String) jsonMap.get(idField);
+                            IndexRequest indexRequest = new IndexRequest(index, documentName, id).source(jsonMap);
+                            bulkRequest.add(new UpdateRequest(index, documentName, id).doc(jsonMap)
+                                    .upsert(indexRequest));
+                            break;
+                        default:
+                            bulkRequest.add(client.prepareIndex(index, mappingType).setSource(jsonMap));
+                            break;
+                    }
                 }
                 i--;
                 BulkResponse bulkResponse = bulkRequest.execute().actionGet();
@@ -128,12 +173,13 @@ public class KafkaElasticSearchSinkTask extends SinkTask {
                 }
             }
         } catch (Exception e) {
-            throw new RetriableException("Elasticsearch not connected",e);
+            throw new RetriableException("Elasticsearch not connected", e);
         }
     }
 
     @Override
-    public void flush(Map<TopicPartition, OffsetAndMetadata> offsets) {}
+    public void flush(Map<TopicPartition, OffsetAndMetadata> offsets) {
+    }
 
     @Override
     public void stop() {
